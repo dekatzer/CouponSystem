821ad10ef5368c9569074dbdd4a4ad4962edcbeb
dekatzer <dekatzer@gmail.com>
2016-07-01 12:39:19 -0700

    1

---
+++ CouponSystem/src/com/coupons/tests/TestCoupons.java
@@ -0,0 +1,17 @@
+package com.coupons.tests;
+import java.util.Date;
+import com.coupons.beans.Coupon;
+import com.coupons.beans.CouponType;
+import com.coupons.dbdao.CouponDBDao;
+
+public class TestCoupons {
+
+	public static void main(String[] args) {
+		
+		CouponDBDao coupdao= new CouponDBDao();
+	//	Coupon coup1=new Coupon(1, "discount", (2016,01,01),(2017,01,01), CouponType.CAMPING, amount, message, price, image);
+		
+
+	}
+
+}
